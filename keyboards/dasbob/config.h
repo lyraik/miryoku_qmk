66df45b31eeeabce3e9b570225a87d8b70d4108e
simon karrer <42744062+lyraik@users.noreply.github.com>
2025-11-02 11:11:41 +0100

    .

--- keyboards/dasbob/config.h
+++ keyboards/dasbob/config.h
@@ -53,7 +53,6 @@
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 250
-#define QUICK_TAP_TERM 120
 #define PERMISSIVE_HOLD
 #define USB_VBUS_PIN 19
 #define AUDIO_PWM_DRIVER PWMD2
